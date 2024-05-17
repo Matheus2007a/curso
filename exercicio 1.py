c26cef3b842664fe9219ffefd48c51a5f71c25ff
Matheus Ferreira Dias <00001116777010sp@aluno.educacao.sp.gov.br>
2024-05-17 17:29:25 -0300

    first commit

---
+++ exercicio 1.py
@@ -0,0 +1,11 @@
+#solicitar ao usuario que digita um numero
+numero = float (input("digite um numero:"))
+#verificar se o numero e positivo, negativo ou zero
+if numero > 0:
+    print("o numero e positivo")
+elif numero < 0:
+     print("o numero e negativo")
+else:
+    print("o numero e zero")
+    
+
